0d38c7fbdd9dc0002ad161dc49b3a5642ee5e6cf
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-12-08 20:25:28 +0100

    add artist resources

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Mousiki.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/artists", ArtistController
   end
 
   # Other scopes may use custom stacks.
