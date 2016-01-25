b3414fabe055e2fa2426894b5a5c93d24624e8aa
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-01-25 23:44:11 +0100

    add configuration to exclude pending tests and show trace

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,5 @@ Mix.Task.run "ecto.create", ["--quiet"]
 Mix.Task.run "ecto.migrate", ["--quiet"]
 Ecto.Adapters.SQL.begin_test_transaction(Mousiki.Repo)
 
+ExUnit.configure exclude: :pending, trace: true
+
