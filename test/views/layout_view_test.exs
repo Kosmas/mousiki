7abfd1e7e6e7a3b37af0fa8c0ac1bbbb4c7bece5
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-01-25 23:29:40 +0100

    add test and links for artists, albums, songs, formats and genres

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,28 @@
 defmodule Mousiki.LayoutViewTest do
   use Mousiki.ConnCase, async: true
+
+  test "contains Artists link" do
+    conn = get conn(), "/"
+    assert html_response(conn, 200) =~ "Artists"
+  end
+
+  test "contains Albums link" do
+    conn = get conn(), "/"
+    assert html_response(conn, 200) =~ "Albums"
+  end
+
+  test "contains Songs link" do
+    conn = get conn(), "/"
+    assert html_response(conn, 200) =~  "Songs"
+  end
+
+  test "contains Formats link" do
+    conn = get conn(), "/"
+    assert html_response(conn, 200) =~ "Formats"
+  end
+
+  test "contains Genre link" do
+    conn = get conn(), "/"
+    assert html_response(conn, 200) =~ "Genres"
+  end
 end
