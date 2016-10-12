a6d220e084e7efff458a3d710382b4e63a7f77fb
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-10-12 23:15:06 +0200

    Update to elixir version 1.3.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mousiki.Mixfile do
   def project do
     [app: :mousiki,
      version: "0.0.1",
-     elixir: "~> 1.3.3",
+     elixir: "~> 1.3.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
